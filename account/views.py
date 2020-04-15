3ad8faf7c8ee6a243bc619bee4d81e15d93f63d8
AndreyS <endry.sh.83.v@gmail.com>
2020-04-15 17:41:39 +0300

    ok 2 comit

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from.forms import LoginForm
@@ -12,15 +12,14 @@ def user_login(request):
             cd = form.cleaned_data
             user = authenticate(request,
                                 username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated '
-                                        'successfully')
-                else:
-                    return HttpResponse('Disabled account')
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('Authenticated successfully')
             else:
-                return HttpResponse('Invalid login')
+                return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
